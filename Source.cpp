49d8af3c2e695badeaf8e2c77f4d8e659426514e
afliwarlok <114007609+afliwarlok@users.noreply.github.com>
2022-09-20 18:32:19 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,142 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	/*int l, m;
+	cin >> l;
+	m = l / 100;
+	cout << m << endl;*/
+	/*int  M, t;
+	cin >> M;
+	t = M / 1000;
+	cout << t << endl;*/
+	/*int f, b;
+	cin >> f;
+	b = f / 1024;
+	cout << b << endl;*/
+	/*int A, B , D;
+	cin >> A >> B;
+	D = A / B;
+	cout << D << endl;*/
+	/*int n;
+	cin >> n;
+	int d = n / 10;
+	int y = n % 10;
+	cout << d << " " << y << endl;*/
+	/*int s;
+	cin >> s;
+	int a = s / 10;
+	int b = s % 10;
+	int h = a + b;
+	cout << a << endl;
+	cout << b << endl;
+	cout << h << endl;*/
+	/*int s;
+	cin >> s;
+	int y = s / 10;
+	int b = s % 10;
+	int D = 10 * b + y;
+	cout << y << endl;
+	cout << b << endl;
+	cout << D << endl;*/
+	/*int s;
+	cin >> s;
+	int D = s / 100;
+	cout << D << endl;*/
+	/*int s;
+	cin >> s;
+	int d = s / 100;
+	int y = (s % 100) / 10;
+	int a = s % 10;
+	cout << a << endl;
+	cout << y << endl;
+	cout << d << endl;*/
+	/*int s;
+	cin >> s;
+	int e = s / 100;
+	int r = (s % 100) / 10;
+	int a = s % 10; 
+	int t = e + r + a;
+	cout << e << endl;
+	cout << r << endl;
+	cout << a << endl;
+	cout << t << endl;*/
+	/*int s;
+	cin >> s;
+	int q = s / 100;
+	int w = (s % 100) / 10;
+	int e = s % 10;
+	int n = 100 * e + 10 * w + q;
+	cout << q << endl;
+	cout << w << endl;
+	cout << e << endl;
+	cout << n <<endl;*/
+	/*int s;
+	cin >> s;
+	int q = s / 100;
+	int w = (s % 100) / 10;
+	int e = s % 10;
+	int n = 100 * w + 10 * e + q;
+	cout << q << endl;
+	cout << w << endl;
+	cout << e << endl;
+	cout << n << endl;*/
+	/*int s;
+	cin >> s;
+	int q = s / 100;
+	int w = (s % 100) / 10;
+	int e = s % 10;
+	int n = 100 * e + 10 * q +w;
+	cout << q << endl;
+	cout << w << endl;
+	cout << e << endl;
+	cout << n << endl;*/
+	/*int s;
+	cin >> s;
+	int q = s / 100;
+	int w = (s % 100) / 10;
+	int e = s % 10;
+	int n = 100 * w+ 10 * q + e;
+	cout << q << endl;
+	cout << w << endl;
+	cout << e << endl;
+	cout << n << endl;*/
+	/*int s;
+	cin >> s;
+	int q = s / 100;
+	int w = (s % 100) / 10;
+	int e = s % 10;
+	int n = 100 * q + 10 * e + w;
+	cout << q << endl;
+	cout << w << endl;
+	cout << e << endl;
+	cout << n << endl;*/
+//int s;
+//cin >> s;
+//int q = s / 1000;
+//int w = (s % 100) / 10;
+//cout << q << endl;
+//cout << w << endl;
+//int s;
+//cin >> s;
+//int q = s / 1000;
+//cout << q << endl;
+//int n;
+//cin  >> n;
+//int min = n / 60;
+//cout << min << endl;
+//int n;
+//cin >> n;
+//int chas = n / 3600;
+//cout << chas << endl;
+
+
+
+
+
+
+
+	
+
+
+
+}
